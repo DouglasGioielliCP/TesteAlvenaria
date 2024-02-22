5e828c9cba76ed33a1d3b7079ba5fdf97a334ee0
Leonardo Gomiero <leoleonardo2000@gmail.com>
2024-02-22 01:30:36 -0300

    Representacao visual esta aparecendo, é preciso separar as paredes adequadamente

--- TesteAlvenaria/Teste/MainTest.cs
+++ TesteAlvenaria/Teste/MainTest.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TesteAlvenaria.Core;
 using TesteAlvenaria.Classes;
+using System.IO;
 
 namespace TesteAlvenaria.Teste;
 
@@ -13,21 +14,82 @@ internal class MainTest
 {
     public static List<IWallData> RunTest(string path)
     {
-        
-        IBlockData bloco1 = new BlockData(40, 0, 000, 0);
-        IBlockData bloco2 = new BlockData(40, 0, 040, 0);
 
         List<IBlockData> blockList = new List<IBlockData>();
-
-        blockList.Add(bloco1);
-        blockList.Add(bloco2);
-
-        IOpeningData open1 = new OpeningData(300, 200, 80, 0);
-
         List<IOpeningData> openingList = new List<IOpeningData>();
 
+        try
+        {
+            if (File.Exists(path))
+            {
+                string[] lines = File.ReadAllLines(path);
 
-        IWallData wall1 = new WallData("Wall1", 0, 0, 90, 500, blockList, openingList);
+                foreach (string line in lines)
+                {
+                    if (line.StartsWith("Bloco"))
+                    {
+                        // Dividindo a linha pelos delimitadores e removendo espaços em branco
+                        string[] values = line.Substring(6).Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+                        int len = int.Parse(values[0]);
+                        int x = int.Parse(values[1]);
+                        int y = int.Parse(values[2]);
+                        int angle = int.Parse(values[3]);
+
+                        int wallPosition = x;
+                        int height = 20;
+                        int elevation = y;
+                        int length = len;
+
+                        // Criando o objeto BlockData e adicionando à lista
+                        IBlockData bloco = new BlockData(wallPosition, height, length, elevation);
+                        blockList.Add(bloco);
+                    }
+                    else if (line.StartsWith("Janela"))
+                    {
+                        // Dividindo a linha pelos delimitadores e removendo espaços em branco
+                        string[] values = line.Substring(7).Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+                        int len = int.Parse(values[0]);
+                        int height = int.Parse(values[1]);
+                        int elevation = int.Parse(values[2]);
+                        int x = int.Parse(values[3]);
+                        int y = int.Parse(values[4]);
+                        int angle = int.Parse(values[5]);
+
+                        IOpeningData janela = new OpeningData(x, height, len, elevation);
+                        openingList.Add(janela);
+                    }
+                    else if (line.StartsWith("Porta"))
+                    {
+                        // Dividindo a linha pelos delimitadores e removendo espaços em branco
+                        string[] values = line.Substring(6).Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+                        int len = int.Parse(values[0]);
+                        int height = int.Parse(values[1]);
+                        int x = int.Parse(values[2]);
+                        int y = int.Parse(values[3]);
+                        int angle = int.Parse(values[4]);
+
+                        IOpeningData porta = new OpeningData(x, height, len, 0);
+                        openingList.Add(porta);
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("O arquivo especificado não foi encontrado.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ocorreu um erro ao ler o arquivo: {ex.Message}");
+        }
+
+
+        IWallData wall1;
+        wall1 = new WallData("Wall1", 0, 0, 90, 500, blockList, openingList);
 
         return new List<IWallData> { wall1 };
     }
