97023903b7dcfd1571e0935c03ce7c70058b5f19
Douglas Gioielli <douglasgioielli@gmail.com>
2024-03-01 19:45:47 -0300

    Inclusão de uma pequena correção para evitar a ocorrência de Exceções na hora de mostrar a primeira ou segunda fiadas.

---
+++ TesteAlvenaria/Teste/MainTest_.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesteAlvenaria.Core;
+
+namespace TesteAlvenaria.Teste;
+
+
+internal class MainTest_
+{
+    public static List<IWallData> RunTest(string path)
+    {
+        return new List<IWallData>();
+    }
+}
